7d6975546b1b7163d520a388a458d522b4cee5e4
tatiane mariano <tatianenmlua@hotmail.com>
2020-07-09 19:35:55 -0300

    ajuste codigo

--- src/main/java/com/votacao/dto/SessaoDTO.java
+++ src/main/java/com/votacao/dto/SessaoDTO.java
@@ -1,5 +1,6 @@
 package com.votacao.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ public class SessaoDTO {
 
     private Long id;
 
+    @JsonFormat(pattern = "dd/MM/yyyy hh:mm:ss")
     private LocalDateTime dataHoraInicio;
 
     private String statusSessao;
